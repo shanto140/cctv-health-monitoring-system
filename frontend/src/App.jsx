d05fde1387f813fdcc5e0406e8272aac5be19b22
Shanto <shantochandraroy140@gmail.com>
2026-08-09 18:23:25 +0600

    feat: add home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+import Home from "./pages/public/Home.jsx";
+
+
 function App() {
   return (
     <Routes>
-       
+       <Route path="/" element={<Home />} />
     </Routes>
   );
 }
