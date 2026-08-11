a47bd5d957e13f526d21f2c370135774e7f29e55
shakil-innovate <shakil.innovate@gmial.com>
2026-08-11 14:20:11 +0600

    add hosting aiven certifiacte to this file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,34 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-dotenv.config();
+  import mysql from 'mysql2/promise';
+  import dotenv from 'dotenv';
+  dotenv.config();
+  import fs from "fs";
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+  const pool = mysql.createPool({
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    ssl: {
+          ca: fs.readFileSync(process.env.DB_SSL_CA)
+      },
 
-export default pool;
+    
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+
+  export const connectDB = async () => {
+    try {
+        const connection = await pool.getConnection();
+        console.log("✅ MySQL Connected");
+        connection.release();
+    } catch (error) {
+        console.error("❌ Database connection failed");
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+  export default pool;
